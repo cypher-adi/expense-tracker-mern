d0e65659f2b34f68f70278048214d0909c38e244
cypher-adi <adityasaha444@gmail.com>
2021-05-23 19:08:16 +0530

    api endpoints added

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -4,3 +4,17 @@
 exports.getTransactions  = (req, res, next) => {
     res.send('GET Transactions');
 }
+
+// @desc    Add transactions
+// @route   POST /api/v1/transactions
+// @access  Public
+exports.addTransactions  = (req, res, next) => {
+    res.send('POST Transactions');
+}
+
+// @desc    Delete transactions
+// @route   Delete /api/v1/transactions/:id
+// @access  Public
+exports.deleteTransactions  = (req, res, next) => {
+    res.send('DELETE Transactions');
+}
--- routes/transactions.js
+++ routes/transactions.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const { getTransactions } = require('../controllers/transactions');
+const { getTransactions, addTransactions, deleteTransactions } = require('../controllers/transactions');
 
 router
     .route('/')
-    .get(getTransactions);
+    .get(getTransactions)
+    .post(addTransactions);
+
+router
+    .route('/:id')
+    .delete(deleteTransactions);
 
 module.exports = router;
